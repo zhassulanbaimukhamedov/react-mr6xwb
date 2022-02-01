fc30f15dd8a021a587e8c3fc18b3764bc83cf88b
zhassulanbaimukhamedov <zasulanb@gmail.com>
2022-02-01 14:11:08 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Let's start</p>
     </div>
   );
 }
